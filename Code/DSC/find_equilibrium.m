0c45984d2ec38c022607151af56f4c7a8738e0e3
bnmurphy <benmurphy@alumni.cmu.edu>
2013-12-05 17:00:48 +0100

    Added equilibrium calculation during initialization

---
+++ Code/DSC/find_equilibrium.m
@@ -0,0 +1,58 @@
+function pv_i = find_equilibrium(T_ref, T_i, DSC, MW, dHvap, pstar, Xm_i, X_i)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file finds an equilibrium state given current conditions           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% T_ref/T_i: Reference/Initial T [K]
+% DSC: structure of calorimeter properties
+% MW(1:nspec): molecular weight [kg/mol]
+% dHvap(1:nspec): enthalpy of varporization [J/mol]
+% pstar(1:nspec): vapor pressure at T_ref
+% Xm_i(1:nspec): Mole fraction of each species assuming STP
+
+R = 8.314; %Gas constant [J mol-1 K-1]
+nspec = length(MW);
+
+%Don't iterate for the equilibirum initially. Just calculate the vapor
+%pressure and subtract from the 
+
+% Equilibrium pressures at the DSC temperature
+psat = pstar.*exp(dHvap.*(1./T_ref - 1./T_i)./R);
+csat(1:nspec) = psat./R./T_i; %Satn Concentration [mol/m3]
+
+%Total Concentration in the cell [mol species / m3 DSC]
+ctot_i(1:nspec) = X_i .* DSC.mass ./ MW ./ DSC.tot_vol;
+
+%Use Bisection to find Coa and psi
+x1 = 0;
+x2 = sum(ctot_i); %Upper Bound on Coa
+i = 0;
+while (x2-x1) > 0.00001 && i < 100
+    xmid = 0.5*(x2+x1);
+    psi2 = 1./(1+csat./xmid);
+    val = sum(ctot_i .* psi2) - xmid;
+    if val < 0
+        x2 = xmid;
+    else
+        x1 = xmid;
+    end
+    i = i + 1;
+end
+cliq = ctot_i.*psi2;      %Total moles of liquid [mol/m3]
+cgas = ctot_i.*(1-psi2);  %Total moles of vapor [mol/m3]
+Xm_i = cliq ./ sum(cliq);  %Mole fraction of each species in the condensed phase
+
+
+%Convert Back to Vapor Pressures
+peq = Xm_i(1:nspec).*psat(1:nspec);
+
+%Equilibrium Gas-Phase Moles of Stuff
+moles_vap = DSC.moles_air + sum(cgas) .* DSC.tot_vol;
+p_i = moles_vap ./ DSC.vap_vol .* R .* T_i;
+
+pv_i = cgas.*DSC.tot_vol./moles_vap .* p_i;  %Initial Partial pressure at equilibrium
+                             %Assume atmospheric pressure first (FIX
+                             %THIS!)
+                             
+                             
+end
--- Code/DSC/fluxes_bulk.m
+++ Code/DSC/fluxes_bulk.m
@@ -1,32 +1,50 @@
-function flx0 = fluxes_bulk(t,input,dt,nspec,Cp_liq,Cp_vap,...
-    pstar,dHvap,T_ref,MW,sigma1,rho,Dn,mu1,press,alpha_m,DSC)
-
-if mod(t,100) == 0, t, end
+function [flx0, p_h2o] = fluxes_bulk(t,input,dt,nspec,Cp_liq,Cp_vap,...
+    pstar,dHvap,T_ref,MW,sigma1,rhol,Dn_air,mu1,alpha_m,DSC)
 
 R = 8.314472;
 %Load Local Variables
 T = input(1);  %Kelvin
-Bc = input(2:nspec+1)';  %Bulk concentration kg
+OLc = input(2:nspec+1)';  %Organic Liquid Bulk concentration kg
 Gc = input(nspec+2:2*nspec+1)';   %Gas concentration kg m-3
 Qin = DSC.Q(find(DSC.time >= t,1 ) );  %J/s
 
 
-
-Bc( Bc <= 0.0 | ~isreal(Bc) ) = 0.0;
+OLc( OLc <= 0.0 | ~isreal(OLc) ) = 0.0;
 Gc( Gc <= 0.0 | ~isreal(Gc) ) = 0.0;
 
+
+%%Caluclate System Properties
+%Recalculate Liquid Volume
+X(1:nspec) = OLc(1:nspec) ./ sum(OLc);  % Mass fraction in the liquid
+rhol_i = sum(X.*rhol); % Mass-weighted average density (kg/m3)
+liq_vol = sum(OLc) ./ rhol_i; %m3
+
+lconstant_volume = 1;
+
+if lconstant_volume
+    vap_vol = DSC.tot_vol - liq_vol;
+    
+    %Pressure at the DSC temperature
+    moles_vap = DSC.moles_air + sum(Gc ./ MW .* vap_vol);
+    press = moles_vap ./ vap_vol .* R .* T;
+else   %Constant Pressure
+    press = 101325;  %Pa
+    
+    %Volume at the DSC Temperature
+    vap_vol = DSC.moles_air ./ (press ./R./T - sum(Gc./MW)); %m3
+end
+
+
+%%Calculate Intensive Properties
 % Equilibrium pressures at the DSC temperature (should be Pa)
 psat(1:nspec) = pstar.*exp(dHvap.*(1./T_ref - 1./T)./R);
 csat(1:nspec) = MW.*psat./R./T;
-
 % Diffusion coefficients of the species
-D(1:nspec) = Dn(1:nspec).*(T/T_ref).^mu1(1:nspec);
+D_air(1:nspec) = Dn_air(1:nspec).*(T/T_ref).^mu1(1:nspec);
 
 % Calculating the composition    
-Bc_tot = sum(Bc);
-if sum(Bc) > 1.0e-22;
-    % Mass fractions of each species in the particles
-    X(1:nspec) = Bc(1:nspec) ./ Bc_tot;
+OLc_tot = sum(OLc);
+if sum(OLc) > 1.0e-22;
     
     % Mole fractions
     n_apu(1:nspec) = X(1:nspec) ./ MW(1:nspec);
@@ -34,19 +52,17 @@ if sum(Bc) > 1.0e-22;
     Xm(1:nspec) = n_apu(1:nspec) ./ n_tot_apu;
     
     % Bulk density (Mass-weighted)
-    rhol = sum(X(1:nspec).*rho(1:nspec));
+    rhol = sum(X(1:nspec).*rhol(1:nspec));
     
     % Bulk surface tension (Mole-weighted)
     sigmal = sum(Xm(1:nspec).*sigma1(1:nspec));
     
     % Bulk volume and size
-    vp = Bc_tot ./ rhol;
+    vp = OLc_tot ./ rhol;
     rp = (3.*vp./4./pi).^(1./3);
     
     % Equilibrium pressures, mole based
-    %peq(j,1:nspec) = Xm(j,1:nspec).*psat(1:nspec);
-    % Mass based! Remember to fix back!!
-    peq(1:nspec) = X(1:nspec).*psat(1:nspec);
+    peq(1:nspec) = Xm(1:nspec).*psat(1:nspec);
     
 else
     mp = 0.0;
@@ -61,50 +77,67 @@ pv_a(1:nspec) = peq;
 
 % Partial pressures far away from the bulk (Pa)
 pv_i(1:nspec) = Gc(1:nspec).*R.*T./MW(1:nspec);
+% pv_i(nspec) = 0;
 
-% Mass flux of each compound to the particles   kg s-1
-b_flx = zeros(1,nspec);
+% Mass flux of each compound between the organic liquid and gas phases   kg s-1
+OL_flx = zeros(1,nspec);
 for i = 1:nspec
     ln_fact = (1.0 - pv_a(i)./press)/(1.0 - pv_i(i)./press);
-    if Bc_tot > 0.0 
+    if OLc_tot > 0.0 
         
         if ln_fact > 0.0  %Evaporation
                           %Stefan (Convective) Flow + Diffusive Flow
             
-            b_flx(i) = DSC.L.*press.*D(i).*MW(i).* ...
-                log((1.0 - pv_a(i)./press)./(1.0 - pv_i(1,i)./press))./R./T;
+            OL_flx(i) = DSC.Area.*D_air(i)./DSC.L .*MW(i).*press./R./T.* ...
+                log((1.0 - pv_a(i)./press)./(1.0 - pv_i(1,i)./press)); %kg/s
             
         else        %Condensation
                     %Just Diffusive flow
             
-            b_flx(i) = DSC.L.*D(i).*MW(i).* ...
-                (pv_i(i)-pv_a(i))./R./T;
+            OL_flx(i) = DSC.Area.*D_air(i)./DSC.L .* ...
+                (pv_i(i)-pv_a(i)) ./R./T.*MW(i); %kg/s
         end
         
     else    %Not enough mass; Do Nothing
-        b_flx(i) = 0.0;
+        OL_flx(i) = 0.0;
     end
 end
 
-% Mass flux of each compound to the gas phase
-g_flx(1:nspec) = -b_flx ./ DSC.tot_vol; %kg m-3 s-1
-
-if T > 373
-    t;
+if OL_flx(nspec) > 0.0
+    OL_flx(nspec);
 end
 
+% Mass flux of each compound to the gas phase
+g_flx(1:nspec) = -OL_flx ./ vap_vol; %kg m-3 s-1
+
+% Calc Water Equilibrium
+mass_eqh2o_vap = peq(nspec) .* vap_vol ./ R ./ T .* MW(nspec); %kg
+mass_eqh2o_liq = DSC.h2o - mass_eqh2o_vap; %kg
+
+
 %Energy Balance (Q is going into the system)
-dT_latent = sum( -b_flx(1:nspec) .* dHvap(1:nspec) ./ MW(1:nspec) ); %J s-1
-dT_Cpliq = sum( Bc(1:nspec) .* Cp_liq(1:nspec) ./ MW(1:nspec) ); %J K-1
+dT_latent = sum( OL_flx(1:nspec) .* dHvap(1:nspec) ./ MW(1:nspec) ); %J s-1
+dT_Cpliq = sum( OLc(1:nspec) .* Cp_liq(1:nspec) ./ MW(1:nspec) ); %J K-1
 dT_Cpvap = sum( Gc(1:nspec) .* DSC.vap_vol .* Cp_vap(1:nspec)./ MW(1:nspec)); %J K-1
-dTdt = ( Qin - dT_latent) / (dT_Cpliq + dT_Cpvap);  %K s-1
+dTdt = ( Qin + dT_latent) / (dT_Cpliq + dT_Cpvap);  %K s-1
+
+
+%Store [Pressure, Press_H2O, Pvap_H2O, EqVapMass_H2O, EqLigMass_H2O,
+%       Flux_H2O, TotLiqVol, TotVapVol]
+p_h2o = [press, pv_i(nspec), pv_a(nspec), ...
+    mass_eqh2o_vap, mass_eqh2o_liq, OL_flx(nspec), liq_vol, vap_vol,...
+    dT_latent];
+fprintf(1,'time: %4.2f  T(degC): %3.2f  H2O_flux(mg/s): %10.3e   LatentHeat: %10.3e   Lat/Q: %10.3e\n',...
+    t, T-273.15, OL_flx(nspec).*1e6,dT_latent,dT_latent/Qin);
+
+
 
 if dTdt ~= dTdt
     pause
 end
 
 % Changing to column vector
-flx0 = [dTdt, b_flx, g_flx]';
+flx0 = [dTdt, OL_flx, g_flx]';
 flx0( ~isreal(flx0) | isnan(flx0) ) = 0.0;
 
 
